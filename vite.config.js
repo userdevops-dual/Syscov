deba99651119d99328713e4874fc7b31f0601c3f
Abdul Raheem <userme782.dev@gmail.com>
2026-01-15 23:33:41 +0500

    Add Vite config in root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 🔑 Netlify ke liye relative paths
+  build: {
+    outDir: 'dist', // ye folder publish directory ke liye
+    sourcemap: false,
+  },
+  server: {
+    open: true,
+  },
 })
